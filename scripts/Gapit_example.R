6555d534861cef8abe83b755c31843fc14d80ab5
Marc Galland <m.galland@uva.nl>
2020-02-24 09:14:09 +0100

    changed update and ask options when installing with biocmanager

--- scripts/Gapit_example.R
+++ scripts/Gapit_example.R
@@ -1,6 +1,6 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install(version = "3.10")
+BiocManager::install(version = "3.10", ask = F, update = F)
 
 if(!require(gplots)) install.packages("gplots")
 if(!require(LDheatmap)) install.packages("LDheatmap")
@@ -12,8 +12,8 @@ if(!require(EMMREML)) install.packages("EMMREML")
 if(!require(scatterplot3d)) install.packages("scatterplot3d")
 
 if(!'multtest'%in% installed.packages()[,"Package"]){
-  BiocManager::install("multtest")
-  BiocManager::install("snpStats")
+  BiocManager::install("multtest", ask = F, update = F)
+  BiocManager::install("snpStats", ask = F, update = F)
 }
 
 
@@ -27,19 +27,19 @@ source("http://www.zzlab.net/GAPIT/gapit_functions.txt")
 myY=read.table(file="http://zzlab.net/GAPIT/data/mdp_traits.txt", head = TRUE)
 myGD=read.table("http://zzlab.net/GAPIT/data/mdp_numeric.txt",head=T)
 myGM=read.table("http://zzlab.net/GAPIT/data/mdp_SNP_information.txt",head=T)
-#myG=read.table(file="http://zzlab.net/GAPIT/data/mdp_genotype_test.hmp.txt", head = FALSE)
+
 # performing simulation phenotype
 set.seed(198521)
 Para=list(h2=0.7,NQTN=20)
 mysimulation<-GAPIT(Para=Para,GD=myGD,GM=myGM)
 myY=mysimulation$Y
 
-#MLM model
+# MLM model
 myGAPIT_MLM <- GAPIT(
-  Y=myY[,c(1,2)],
-  GD=myGD,
-  GM=myGM,
-  model="MLM",
-  PCA.total=5,
-  file.output=T
+  Y = myY[,c(1,2)],
+  GD = myGD,
+  GM = myGM,
+  model = "MLM",
+  PCA.total = 5,
+  file.output = TRUE
 )
