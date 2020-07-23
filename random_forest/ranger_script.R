c8bc3ed51085a203bbe41fa7da1e830209bda6d7
Marc Galland <m.galland@uva.nl>
2020-07-23 14:45:42 +0200

    changed formula to dependent variable in ranger call

--- random_forest/ranger_script.R
+++ random_forest/ranger_script.R
@@ -1,26 +1,59 @@
 # https://uc-r.github.io/random_forests
 
-checkpoint::checkpoint("2020-06-01")
-library(rsample)      # data splitting 
-library(randomForest) # basic implementation
-library(ranger)       # a faster implementation of randomForest
-library(caret)        # an aggregator package for performing many machine learning models
-library(tibble)
-library(vcfR)
+if ("devtools" %in% installed.packages()){
+  library("devtools")
+  } else {
+    install.packages("devtools")
+  }
 
-###################################
+if ("vcfR" %in% installed.packages()){
+  library("vcfR")
+} else {
+  devtools::install_version('vcfR', version = '1.10.0', upgrade = "never" )
+}
+
+if ("ranger" %in% installed.packages()){
+  library("ranger")
+} else {
+  devtools::install_version('ranger', version = '0.12.0', upgrade = "never") 
+}
+
+if ("tidyverse" %in% installed.packages()){
+  library("tidyverse")
+} else {
+  devtools::install_version('tidyverse', version = '1.2.0', upgrade = "never") 
+}
+
+
+#checkpoint::checkpoint("2020-06-01")
+#library(rsample)      # data splitting 
+#library(randomForest) # basic implementation
+#library(ranger)       # a faster implementation of randomForest
+#library(caret)        # an aggregator package for performing many machine learning models
+#library(tibble)
+
+
+#library(dplyr)
+
+##############################
 # Import VCF file transformed  
-# Alleles converted to binary data (0, 1, 2)
-###########################################
+# Convert genotypes to alleles
+##############################
 
-vcf <- read.vcfR("data/chr01.header.Arabidopsis_2029_Maf001_Filter80.vcf.gz", verbose = FALSE, nrows = 1000)
+vcf <- read.vcfR("data/chr01.header.Arabidopsis_2029_Maf001_Filter80.vcf.gz", 
+                 verbose = FALSE, 
+                 nrows = 1000, 
+                 convertNA = TRUE, 
+                 checkFile = TRUE)
 
 # Extract genotype information
+# Rows: individuals
+# Columns: SNPs
 genotypes <- extract.gt(vcf, 
                         return.alleles = TRUE, 
                         IDtoRowNames = TRUE, 
                         convertNA = TRUE) %>% 
-  t(.) %>% 
+  t(.) %>%                            
   as.data.frame() %>% 
   rownames_to_column("FID")
 
@@ -32,8 +65,7 @@ pheno <- na.omit(read.delim(file = "data/root_data_fid_and_names.tsv",
                             stringsAsFactors = FALSE, 
                             colClasses = c("character","numeric","character")
                             )
-                 )
-pheno <- pheno %>% 
+                 ) %>% 
   dplyr::select(FID, Ratio) %>% 
   remove_rownames() 
 
@@ -41,18 +73,21 @@ pheno <- pheno %>%
 # Create object ready for Random Forest
 #######################################
 
-df <- dplyr::inner_join(genotypes, pheno, by = "FID") %>% 
-  select(- FID)
+df <- dplyr::inner_join(genotypes, pheno, by = "FID") 
 
 # to ensure compatibility with formula (numbers not allowed for columns)
 names(df) <- make.names(names(df), allow_ = FALSE) 
 
+
+######################
+# Ranger Random Forest
+######################
 # hyperparameter grid search --> same as above but with increased mtry values
 hyper_grid <- expand.grid(
-  ntree     = seq(500, 1000, by = 100),
-  mtry       = seq(50, 200, by = 25),
-  node_size  = seq(3, 9, by = 2),
-  sample_size = c(.55, .632, .70, .80),
+  ntree     = seq(1000, 10000, by = 1000),
+  #mtry       = seq(50, 200, by = 25),
+  #node_size  = seq(3, 9, by = 2),
+  sample_size = c(.60, .70, .80),
   OOB_RMSE  = 0
 )
 
@@ -61,24 +96,24 @@ for(i in 1:nrow(hyper_grid)) {
   
   # train model
   rf_model <- ranger(
-    formula         = Ratio ~ ., 
-    data            = df, 
-    num.trees       = hyper_grid$ntree[i],
-    mtry            = hyper_grid$mtry[i],
-    min.node.size   = hyper_grid$node_size[i],
-    sample.fraction = hyper_grid$sample_size[i],
-    seed            = 123
+    dependent.variable.name = "Ratio",
+    data                    = df, 
+    num.trees               = hyper_grid$ntree[i],
+    #mtry                    = hyper_grid$mtry[i],
+    #min.node.size           = hyper_grid$node_size[i],
+    sample.fraction         = hyper_grid$sample_size[i],
+    seed                    = 123
   )
   
   # add OOB error to grid
   hyper_grid$OOB_RMSE[i] <- sqrt(rf_model$prediction.error)
 }
 
-
+# extract the best parameters
 best_params <- hyper_grid[which.min(hyper_grid$OOB_RMSE),]
 
 rf_optimised_model <- ranger(
-  formula = Ratio ~ .,
+  dependent.variable.name = "Ratio",
   data = df,
   num.trees = best_params$ntree,
   mtry = best_params$mtry,
